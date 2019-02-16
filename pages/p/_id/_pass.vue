fbf1217810f75915aa30484eac469ffbaf9f7e9c
松田 有人 <arihito.m@gmail.com>
2019-02-16 21:46:39 +0900

    validate directive

--- pages/p/_id/_pass.vue
+++ pages/p/_id/_pass.vue
@@ -18,16 +18,16 @@ export default {
   components: {
     Logo
   },
+  validate ({ params }) {
+    if (params.id == undefined || params.pass == undefined) {
+      return false
+    } else {
+      return true
+    }
+  },
   data: function() {
     return {
       title: 'Login'
-    }
-  },
-  computed: {
-    message: function () {
-      let id = this.$route.params.id != undefined ? this.$route.params.id : '*** no id ***'
-      let pass = this.$route.params.pass != undefined ? this.$route.params.pass : '*** no password ***'
-      return 'ID: ' + id + '<br>PASS:' + pass
     }
   }
 }
