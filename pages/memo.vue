8ab750d1efb215bfe24efb27a063a81d0a63a539
松田 有人 <arihito.m@gmail.com>
2019-02-19 21:37:27 +0900

    add edit button & style

--- pages/memo.vue
+++ pages/memo.vue
@@ -11,11 +11,11 @@
           </th>
           <td class="field">
             <div class="control">
-              <input type="text" v-model="title" @focus="set_flg" class="input is-success" name="title">
+              <input type="text" v-model="title" @focus="setting_flg" class="input is-success" name="title">
             </div>
           </td>
           <td>  
-            <button @click="find" class="button--grey6">検索</button>
+            <button @click="find" v-if="sel_flg == false" class="button--grey6">検索</button>
           </td>
         </tr>
         <tr>
@@ -24,18 +24,24 @@
           </th>
           <td class="field">
             <div class="control">
-              <textarea v-model="content" class="textarea is-success" name='content' cols='50' rows='5'></textarea>
+              <textarea v-model="content" class="textarea is-success" name='content' cols='50' rows='6'></textarea>
             </div>
           </td>
           <td>
             <button @click="insert" class="button--grey6">保存</button><br>
-            <transition name="del">
+            <transition name="btn">
+              <button v-if="sel_flg != false" @click="edit" class="button--grey6">更新</button>
+            </transition>
+            <br>
+            <transition name="btn">
               <button v-if="sel_flg != false" @click="remove" class="button--grey6">削除</button>
             </transition>
           </td>
         </tr>
         <tr>
-          <td></td>
+          <td>
+            <router-link to="/" class="button--green">Go to Top</router-link>      
+          </td>
           <td>      
             <ul class="archive">
               <li v-for="item in page_items" :key="item.title">
@@ -43,12 +49,19 @@
               </li>
             </ul>
             <div class='nav'>
-              <span @click="prev" class="button--grey6">＜ 新しい一覧</span>
-              <router-link to="/" class="button--green">Go to Top</router-link>      
-              <span @click="next" class="button--grey6">過去の一覧 ＞</span>
+              <transition name="btn">
+                <span @click="prev" v-if="sel_flg == false" class="button--grey6">＜ 新しい一覧</span>
+              </transition>
+              <transition name="btn">
+                <span @click="next" v-if="sel_flg == false" class="button--grey6">過去の一覧 ＞</span>
+              </transition>
             </div>
           </td>
-          <td></td>
+          <td>
+            <transition name="btn">
+              <button v-if="sel_flg != false" @click="setting_flg" class="button--grey6">戻る</button>
+            </transition>
+          </td>
         </tr>
       </table>
     </div>
@@ -103,14 +116,16 @@ export default {
         return this.$store.state.memo.page
       },
       set: function (p) {
-        let pg = p > (this.$store.state.memo.memo.length -1) / this.num_per_page ? Math.ceil((this.$store.state.memo.memo.length -1) / this.num_per_page) - 1 : p
+        // もし登録件数-1を表示件数でわったとき今のページ数より小さければ切り上げたページ数を代入、大きければそのまま代入
+        let pg = p > (this.$store.state.memo.memo.length - 1) / this.num_per_page ? Math.ceil((this.$store.state.memo.memo.length -1) / this.num_per_page) - 1 : p
+        // マイナスの場合は0そうでなければそのまま
         pg = pg < 0 ? 0 : pg
         this.$store.commit('memo/set_page', pg)
       }
     }
   },
   methods: {
-    set_flg: function () {
+    setting_flg: function () {
       if (this.find_flg || this.sel_flg != false) {
         this.find_flg = false
         this.sel_flg  = false
@@ -125,17 +140,26 @@ export default {
     },
     select: function (item) {
       this.find_flg = false
-      this.set_flg  = item
+      this.sel_flg  = item
       this.title    = item.title
       this.content  = item.content
     },
+    edit: function () {
+      if (this.sel_flg == false) {
+        return
+      }
+      else {
+        this.$store.commit('memo/edit', {select: this.sel_flg, input: {title:this.title, content:this.content}})
+        this.setting_flg()
+      }
+    },
     remove: function () {
-      if (this.set_flg == false) {
+      if (this.sel_flg == false) {
         return
       }
       else {
         this.$store.commit('memo/remove', this.sel_flg)
-        this.set_flg()
+        this.setting_flg()
       }
     },
     find: function () {
--- store/memo.js
+++ store/memo.js
@@ -7,11 +7,11 @@ export const mutations = {
 
   // 記事情報を配列の手前に追加
   insert: function (state, obj) {
-    let d = new Date()
-    let w = d.getDay()
-    const wja = ['日', '月', '火', '水', '木', '金', '土']
-    let fmt = d.getFullYear() + '年' + (d.getMonth() + 1) + '月' + d.getDate() + '日(' + wja[w] + ') ' + d.getHours() + '時' + d.getMinutes() + '分'
-    state.memo.unshift({
+  let d = new Date()
+  let w = d.getDay()
+  const wja = ['日','月','火','水','木','金','土']
+  const fmt = d.getFullYear() + '年' + (d.getMonth() + 1) + '月' + d.getDate() + '日(' + wja[w] + ') ' + d.getHours() + '時' + d.getMinutes() + '分'
+  state.memo.unshift({
       title: obj.title,
       content: obj.content,
       created: fmt
@@ -22,14 +22,31 @@ export const mutations = {
   set_page: function (state, p) {
     state.page = p
   },
-
+  // 記事件数分の中で選択したタイトル/本文/日付と等しい記事を編集
+  edit: function (state, obj) {
+    let d = new Date()
+    let w = d.getDay()
+    const wja = ['日', '月', '火', '水', '木', '金', '土']
+    let fmt = d.getFullYear() + '年' + (d.getMonth() + 1) + '月' + d.getDate() + '日(' + wja[w] + ') ' + d.getHours() + '時' + d.getMinutes() + '分'
+    let newObj = {
+      title: obj.input.title,
+      content: obj.input.content,
+      created: fmt
+    }
+    for (let i = 0; i < state.memo.length; i++) {
+      const ob = state.memo[i]
+      if (ob.title == obj.select.title && ob.content == obj.select.content && ob.created == obj.select.created) {
+        // 指定したインデックス番号の配列要素を1件置換
+        state.memo.splice(i, 1, newObj)
+        return
+      }
+    }
+  },
   // 記事件数分の中で選択したタイトル/本文/日付と等しい記事を削除
   remove: function (state, obj) {
-    let num = 0
     for (let i = 0; i < state.memo.length; i++) {
       const ob = state.memo[i]
       if (ob.title == obj.title && ob.content == obj.content && ob.created == obj.created) {
-        alert('remove it ! --' + ob.title)
         // 指定したインデックス番号の配列要素を1件削除
         state.memo.splice(i, 1)
         return
